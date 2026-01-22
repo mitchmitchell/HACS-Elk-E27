2a2bc895d5fbaa0d8f6465c9008127f739be7bcb
mitchmitchell <mitchmitchell1616@msn.com>
2026-01-21 20:39:04 -0600

    update elkm1 library to elke27 library

--- custom_components/elke27/vendor/elkm1/elke27_lib/generators/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Outbound request generators and registry."""
-
-from .registry import COMMANDS, CommandSpec
-
-__all__ = ["CommandSpec", "COMMANDS"]
--- custom_components/elke27/vendor/elkm1/elkm1_lib/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ElkM1 library"""
-
-from .elk import Elk
-
-__all__ = ["Elk"]
